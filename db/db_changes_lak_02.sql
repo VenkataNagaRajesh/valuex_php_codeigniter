91d7439973a65901f8baf29e2c12804c5498dfcf
lakshmi <lakshmi@DESKTOP-Lakshmi>
2019-05-04 17:28:06 +0530

    db changes market type

---
+++ db/db_changes_lak_02.sql
@@ -0,0 +1,3 @@
+insert into vx_aln_data_types (name,alias,create_userID,modify_userID,create_date,modify_date) values ('aln_market_zone','Market Zone', '1','1','1554788423','1554788423');
+
+drop table VX_aln_airline;
