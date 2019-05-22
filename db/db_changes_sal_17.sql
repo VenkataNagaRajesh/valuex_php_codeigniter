4ff14ae4c71b5422c88ce6cd52dbc3872fec5f9e
salini.gunturu@sweken.com <salini.gunturu@sweken.com>
2019-05-22 10:59:29 +0530

    adding code for days of week

---
+++ db/db_changes_sal_17.sql
@@ -0,0 +1,7 @@
+ update vx_aln_data_defns set code = '1'  where aln_data_value = 'Monday' and aln_data_typeID = 14;
+ update vx_aln_data_defns set code = '2'  where aln_data_value = 'Tuesday' and aln_data_typeID = 14;
+ update vx_aln_data_defns set code = '3'  where aln_data_value = 'Wednesday' and aln_data_typeID = 14;
+ update vx_aln_data_defns set code = '4'  where aln_data_value = 'Thursday' and aln_data_typeID = 14;
+ update vx_aln_data_defns set code = '5'  where aln_data_value = 'Friday' and aln_data_typeID = 14;
+ update vx_aln_data_defns set code = '6'  where aln_data_value = 'Saturday' and aln_data_typeID = 14;
+ update vx_aln_data_defns set code = '0'  where aln_data_value = 'Sunday' and aln_data_typeID = 14;
