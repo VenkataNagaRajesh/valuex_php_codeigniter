48d786ca7dc8aa39b10915bfce4f3eb12dbb4c0b
salini.gunturu@sweken.com <salini.gunturu@sweken.com>
2019-05-28 15:08:03 +0530

    adding offer issue code and sending mail

---
+++ mvc/models/Offer_issue_m.php
@@ -0,0 +1,62 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Offer_issue_m extends MY_Model {
+
+	protected $_table_name = 'VX_aln_dtpf_tracker';
+	protected $_primary_key = 'dtpf_tracker_id';
+	protected $_primary_filter = 'intval';
+	protected $_order_by = "dtpf_tracker_id desc";
+
+        function __construct() {
+                parent::__construct();
+        }
+
+        function get_dtpf_tracker($array=NULL, $signal=FALSE) {
+                $query = parent::get($array, $signal);
+                return $query;
+        }
+
+
+        function get_single_dtpf_tracker($array=NULL) {
+                $query = parent::get_single($array);
+                return $query;
+        }
+
+        function insert_dtpf_tracker($array) {
+		$error = parent::insert($array);
+                return TRUE;
+
+        }
+
+	  function update_dtpf_tracker($data, $id = NULL) {
+                parent::update($data, $id);
+                return $id;
+        }
+
+
+	function checkForUniqueCouponCode($code) {
+		$this->db->select('dtpfext_id')->from('VX_aln_dtpf_ext');
+		$this->db->where('coupon_code',$code);
+		$this->db->limit(1);
+		$query = $this->db->get();
+                $check = $query->row();
+		if($check->dtpfext_id) {
+                    return false ;
+                } else {
+                  return true;
+                }
+
+	}
+
+
+        public function delete_dtpf_tracker($id){
+                parent::delete($id);
+        }
+
+	function hash($string) {
+                return parent::hash($string);
+        }
+
+	
+}
+
