2592ee288bfc146a683507dcc6318f2e5fd31909
Anitha Jeereddi <anitha.jeereddi@sweken.com>
2019-05-30 15:25:23 +0530

    valuex code updated

---
+++ mvc/views/_layout_home.php
@@ -0,0 +1,46 @@
+<?php echo doctype("html5"); ?>
+<html class="white-bg-login" lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta charset="UTF-8">
+	<title><?php echo $this->lang->line("panel_title");?></title>	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="Title" content="AAASOM - All Adaptive services for Home or Office or Organization,All services" />
+	<meta name="description" content="Singapore Airlines...."/>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8">
+	<meta property="fb:app_id" content="254488398564329"/>
+	<!--Twitter-->
+	<link href="<?php echo base_url('assets/home/css/animate.css'); ?>" rel="stylesheet">
+	<link href="<?php echo base_url('assets/home/css/icomoon.css'); ?>" rel="stylesheet">
+	<link href="<?php echo base_url('assets/home/css/bootstrap.css'); ?>" rel="stylesheet">
+	<link href="<?php echo base_url('assets/home/css/superfish.css'); ?>" rel="stylesheet">
+	<link href="<?php echo base_url('assets/home/css/magnific-popup.css'); ?>" rel="stylesheet">
+	<link href="<?php echo base_url('assets/home/css/bootstrap-datepicker.min.css'); ?>" rel="stylesheet">
+	<link href="<?php echo base_url('assets/home/css/cs-select.css'); ?>" rel="stylesheet">
+	<link href="<?php echo base_url('assets/home/css/cs-skin-border.css'); ?>" rel="stylesheet">
+	<link href="<?php echo base_url('assets/home/css/style.css'); ?>" rel="stylesheet">
+
+</head>	
+
+<body>
+
+	<?php $this->load->view($subview); ?>
+	
+	<script type="text/javascript" src="<?php echo base_url('assets/home/js/modernizr-2.6.2.min.js');?>"></script>
+	<script type="text/javascript" src="<?php echo base_url('assets/home/js/jquery.min.js');?>"></script>	
+	<script type="text/javascript" src="<?php echo base_url('assets/home/js/jquery.easing.1.3.js');?>"></script>	
+	<script type="text/javascript" src="<?php echo base_url('assets/home/js/bootstrap.min.js');?>"></script>	
+	<script type="text/javascript" src="<?php echo base_url('assets/home/js/jquery.waypoints.min.js');?>"></script>
+	<script type="text/javascript" src="<?php echo base_url('assets/home/js/sticky.js');?>"></script>	
+	<script type="text/javascript" src="<?php echo base_url('assets/home/js/jquery.stellar.min.js');?>"></script>
+	<script type="text/javascript" src="<?php echo base_url('assets/home/js/hoverIntent.js');?>"></script>
+	<script type="text/javascript" src="<?php echo base_url('assets/home/js/superfish.js');?>"></script>	
+	<script type="text/javascript" src="<?php echo base_url('assets/home/js/jquery.magnific-popup.min.js');?>"></script>
+	<script type="text/javascript" src="<?php echo base_url('assets/home/js/magnific-popup-options.js');?>"></script>	
+	<script type="text/javascript" src="<?php echo base_url('assets/home/js/bootstrap-datepicker.min.js');?>"></script>
+	<script type="text/javascript" src="<?php echo base_url('assets/home/js/classie.js');?>"></script>
+	<script type="text/javascript" src="<?php echo base_url('assets/home/js/selectFx.js');?>"></script>
+	<script type="text/javascript" src="<?php echo base_url('assets/home/js/main.js');?>"></script>
+	<script src='https://www.google.com/recaptcha/api.js'></script>
+</body>
+</html>
---
+++ mvc/views/home/index.php
@@ -0,0 +1,93 @@
+<div id="fh5co-wrapper">
+	<div id="fh5co-page">
+		<!--<div class="container">
+			<div class="row">
+				<div class="col-md-1">
+					<img src="" alt="emirtaes" class="img-responsive">
+				</div>
+				<div class="col-md-1 pull-right">
+					<p> 
+						<select class="form-control">
+							<i class="fa fa-world"></i>
+							<option value="0">IN</option>
+							<option value="1">USA</option>
+							<option value="2">UK</option>
+						</select>
+					</p>
+				</div>
+			</div>
+		</div>-->
+		<div class="fh5co-hero">
+			<div class="fh5co-cover container">
+				<img class="img-responsive" src="<?php echo base_url('assets/home/images/login-bg.png'); ?>" alt="About Our services">
+			</div>
+		</div>
+		<div class="container pnr-box">
+			<div class="row">
+				<div class="col-sm-5 col-md-4 col-md-offset-2">
+					<div class="tabulation animate-box">
+						<h2>Ugrade for Sure</h2>
+					</div>
+				</div>
+				<div class="col-sm-5 col-md-4">
+					<div class="tabulation animate-box">
+						<iframe src="https://www.youtube.com/embed/_O2_nTt1N6w" width="100%" height="180"></iframe>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2">
+					<div class="pnr-form">
+						<form class="form-horizontal" action="/action_page.php">
+							<div class="form-group">
+								<div class="col-sm-5 col-md-offset-1">
+									<input type="text" class="form-control" id="pnr" placeholder="PNR">
+								</div>
+								<div class="col-sm-5">
+									<input type="text" class="form-control" id="code" placeholder="Code">
+								</div>
+							</div>
+							<div class="captcha">
+								<div class="col-sm-3 col-md-offset-3">
+									<label><input type="checkbox" value=""> im not a Robot </label>
+								</div>
+								<div class="col-md-6">
+									<h2>Captcha</h2>
+								</div>
+							</div>
+							<div class="form-group">
+								<div class="col-md-4 col-md-offset-4">
+									<button type="button" class="btn btn-dander btn-lg">Proceed</button>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+					<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+						<div class="panel panel-default">
+							<div class="panel-heading" role="tab" id="headingOne">
+								<h4 class="panel-title">
+									<a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+										<i class="more-less glyphicon glyphicon-chevron-down" aria-hidden="true"></i>
+										Upgrade for Sure "UFS" Offer for INternational and Domestic Passengers
+									</a>
+								</h4>
+							</div>
+							<div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+								<div class="panel-body">
+									  Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+									  Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+								</div>
+							</div>
+						</div>
+					</div><!-- panel-group -->	
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
