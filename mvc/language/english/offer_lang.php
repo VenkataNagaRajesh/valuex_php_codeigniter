08de451bc1c6543f7dc3dc1432da8305755e9bfa
salini.gunturu@sweken.com <salini.gunturu@sweken.com>
2019-06-01 16:49:14 +0530

    adding lang file for offer page

---
+++ mvc/language/english/offer_lang.php
@@ -0,0 +1,36 @@
+<?php
+
+/* List Language  */
+$lang['panel_title'] = "Offer Management";
+$lang['slno'] = "#";
+$lang['action'] = "Action";
+$lang['active'] = 'Status';
+
+$lang['offer_issue'] = 'Offer Issue';
+$lang['edit'] = 'Edit';
+$lang['delete'] = 'Delete';
+$lang['fclr_id'] = 'FCLR ID';
+$lang['pax_id'] = 'Pax feed ID';
+$lang['booking_status'] = 'Booking Status';
+$lang['offer_id'] = 'Offer ID';
+$lang['pnr_ref'] = 'PNR Reference';
+$lang['passenger_list'] = 'Passenger List'; 
+$lang['generate_offer_eligibility'] = 'Generate Offer Eligibility Data';
+$lang['view'] = 'View';
+$lang['avg'] = 'Average';
+$lang['min'] = 'Minimum';
+$lang['max'] = 'Maximum';
+$lang['slider_start'] = 'Slider Position';
+$lang['season'] = 'Season';
+$lang['flight_number'] = 'Flight Number';
+$lang['carrier'] = 'Carrier';
+$lang['board_point'] = 'Board Point';
+$lang['off_point'] = 'Off Point';
+$lang['from_cabin'] = 'From Cabin';
+$lang['to_cabin'] = 'To Cabin';
+$lang['day_of_week'] = 'Frequency';
+$lang['days_to_departure'] = 'Days to Departure';
+$lang['departure_date'] = 'Departure Date';
+$lang['flight_number'] = 'Flight Number';
+$lang['operating_airline_code'] = 'Operating Airline';
+$lang['marketing_airline_code'] = 'Marketing Airline';
