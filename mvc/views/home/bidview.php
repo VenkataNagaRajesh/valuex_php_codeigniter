32119a94f224bc577917a2773a45a3e73e035bd0
Anitha Jeereddi <anitha.jeereddi@sweken.com>
2019-06-04 18:01:11 +0530

    vlaues bid screen added

---
+++ mvc/views/home/bidview.php
@@ -0,0 +1,285 @@
+<div class="container">
+	<!--<div class="row">
+		<div class="col-md-12 top-bar">
+			<div class="col-md-1">
+				<img src="" alt="emirtaes" class="img-responsive">
+			</div>
+			<div class="col-md-1 pull-right">
+				<select class="form-control">
+					<i class="fa fa-world"></i>
+					<option value="0">IN</option>
+					<option value="1">USA</option>
+					<option value="2">UK</option>
+				</select>
+			</div>
+		</div>
+	</div>-->
+	<div class="row">
+		<div class="col-md-12">
+			<div class="bid-tab">
+				<ul class="nav nav-tabs">
+					<li class="active col-md-6"><a data-toggle="tab" href="#offer"><span class="badge badge-secondary">1</span> Make Us ana Offer</a></li>
+					<li class="col-md-6"><a data-toggle="tab" href="#payment"><span class="badge badge-secondary">2</span> Review & Payment</a></li>
+				</ul>
+				 <div class="tab-content">
+					<div id="offer" class="tab-pane fade in active">
+						<div class="col-md-12">
+							<h3>your chance for a Emirates business upgrade</h3>
+							<p>Treat yourself to the amenities and comfort of business upgrade to business class by submitting your bid here is how to proceed</p>
+							<ol>
+								<li>Submit your bid per flight leg and click on next</li>
+								<li>Indicate your method of payment you will be charged only if your upgrade bid has been approved</li>
+								<li>Check and confirm your data</li>
+							</ol>
+							<p>The submission of your bid will be confirmed in a sepate email. you will receive a message from us between 120 and 48 hours before departure informing you whether or not you bid has been accepted, Bids are per flight segment and donot including the original ticket price</p>
+						</div>
+						<div class="col-md-10">
+							<div class="pass-info">
+								<p>Passenger(s):<span>Mr.Ram kishore, Mr.Vamsi, Mr.Suraj, Mr.Shyam</span>
+								<span class="pull-right" style="color:#333;">Booking Ref No: 12346789</span></p>
+								<div class="col-md-5">
+									<h2>Flight Information</h2>
+									<div class="bid-info">
+										<div class="col-md-6">
+											<p>Los Angeles</p>
+											<ul>
+												<li>02 Jan 2016</li>
+												<li>Flight LX 41</li>
+												<li>11 hours 15 mins</li>
+												<li>02.30am-15.25pm</li>
+											</ul>
+										</div>
+										<p><i class="fa fa-fighter-jet"></i></p>
+										<div class="col-md-6">
+											<p>Zurich (ZRH)</p>
+											<ul>
+												<li>02 Jan 2016</li>
+												<li>Flight LX 41</li>
+												<li>11 hours 15 mins</li>
+												<li>02.30am-15.25pm</li>
+											</ul>
+										</div>
+									</div>
+									<div class="bid-info bid-visible">
+										<div class="col-md-6">
+											<p>Los Angeles</p>
+											<ul>
+												<li>02 Jan 2016</li>
+												<li>Flight LX 41</li>
+												<li>11 hours 15 mins</li>
+												<li>02.30am-15.25pm</li>
+											</ul>
+										</div>
+										<p><i class="fa fa-fighter-jet"></i></p>
+										<div class="col-md-6">
+											<p>Zurich (ZRH)</p>
+											<ul>
+												<li>02 Jan 2016</li>
+												<li>Flight LX 41</li>
+												<li>11 hours 15 mins</li>
+												<li>02.30am-15.25pm</li>
+											</ul>
+										</div>
+									</div>
+									<div class="bid-info">
+										<div class="col-md-6">
+											<p>Los Angeles</p>
+											<ul>
+												<li>02 Jan 2016</li>
+												<li>Flight LX 41</li>
+												<li>11 hours 15 mins</li>
+												<li>02.30am-15.25pm</li>
+											</ul>
+										</div>
+										<p><i class="fa fa-fighter-jet"></i></p>
+										<div class="col-md-6">
+											<p>Zurich (ZRH)</p>
+											<ul>
+												<li>02 Jan 2016</li>
+												<li>Flight LX 41</li>
+												<li>11 hours 15 mins</li>
+												<li>02.30am-15.25pm</li>
+											</ul>
+										</div>
+									</div>
+									<div class="bid-info">
+										<div class="col-md-6">
+											<p>Los Angeles</p>
+											<ul>
+												<li>02 Jan 2016</li>
+												<li>Flight LX 41</li>
+												<li>11 hours 15 mins</li>
+												<li>02.30am-15.25pm</li>
+											</ul>
+										</div>
+										<p><i class="fa fa-fighter-jet"></i></p>
+										<div class="col-md-6">
+											<p>Zurich (ZRH)</p>
+											<ul>
+												<li>02 Jan 2016</li>
+												<li>Flight LX 41</li>
+												<li>11 hours 15 mins</li>
+												<li>02.30am-15.25pm</li>
+											</ul>
+										</div>
+									</div>
+								</div>
+								<div class="col-md-3">
+									<h2>Upgrade Type</h2>
+									<div class="bid-radio">
+										<label class="radio-inline">
+											<input type="radio" name="optradio">Premium
+										</label><br>
+										<label class="radio-inline">
+											<input type="radio" name="optradio">Business
+										</label><br>
+										<label class="radio-inline">
+											<input type="radio" name="optradio">No Bid
+										</label>
+									</div>
+									<div class="bid-radio bid-visible">
+										<label class="radio-inline">
+											<input type="radio" name="optradio">Premium
+										</label><br>
+										<label class="radio-inline">
+											<input type="radio" name="optradio">Business
+										</label><br>
+										<label class="radio-inline">
+											<input type="radio" name="optradio">No Bid
+										</label>
+									</div>
+									<div class="bid-radio">
+										<label class="radio-inline">
+											<input type="radio" name="optradio">Premium
+										</label><br>
+										<label class="radio-inline">
+											<input type="radio" name="optradio">Business
+										</label><br>
+										<label class="radio-inline">
+											<input type="radio" name="optradio">No Bid
+										</label>
+									</div>
+									<div class="bid-radio">
+										<label class="radio-inline">
+											<input type="radio" name="optradio">Premium
+										</label><br>
+										<label class="radio-inline">
+											<input type="radio" name="optradio">Business
+										</label><br>
+										<label class="radio-inline">
+											<input type="radio" name="optradio">No Bid
+										</label>
+									</div>
+								</div>
+								<div class="col-md-4">
+									<h2>Bid (s)</h2>
+									<div class="price-range">
+										<p>Price Range</p>
+									</div>
+									<div class="price-range bid-visible">
+										<p>Price Range</p>
+									</div>
+									<div class="price-range">
+										<p>Price Range</p>
+									</div>
+									<div class="price-range">
+										<p>Price Range</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-2">
+							<div class="side-image">
+								<img class="img-responsive img-thumbnail" src="<?php echo base_url('assets/home/images/multi-bid/mb1.jpg'); ?>" alt="img1">
+							</div>
+							<div class="side-image">
+								<img class="img-responsive img-thumbnail" src="<?php echo base_url('assets/home/images/multi-bid/mb2.jpg'); ?>" alt="img1">
+							</div>
+							<div class="side-image">
+								<img class="img-responsive img-thumbnail" src="<?php echo base_url('assets/home/images/multi-bid/mb3.jpg'); ?>" alt="img1">
+							</div>
+							<div class="side-image">
+								<img class="img-responsive img-thumbnail" src="<?php echo base_url('assets/home/images/multi-bid/mb1.jpg'); ?>" alt="img1">
+							</div>
+							<div class="side-image">
+								<img class="img-responsive img-thumbnail" src="<?php echo base_url('assets/home/images/multi-bid/mb2.jpg'); ?>" alt="img1">
+							</div>
+							<div class="side-image">
+								<img class="img-responsive img-thumbnail" src="<?php echo base_url('assets/home/images/multi-bid/mb3.jpg'); ?>" alt="img1">
+							</div>
+						</div>
+					</div>
+					<div id="payment" class="tab-pane fade">
+						<div class="col-md-9">
+							<div class="col-md-2 back-btn">
+								<a type="button" class="btn btn-danger"><i class="fa fa-arrow-left"></i> Back</a>
+							</div>
+							<div class="col-md-10 booking-ref">
+								<h2 class="pull-right">Booking Ref No: 12346789</h2>
+							</div>
+							<div class="col-md-12">
+								<p class="pull-right">Total Bid Amount  <b style="margin-left:12px;"><i class="fa fa-dollar"></i> 2100.00</b></p>
+							</div>
+							<div class="col-md-8 col-md-offset-2">
+								<div class="price-range">
+									<p>Price Range</p>
+								</div>
+							</div>
+							<div class="col-md-12 payment-box">
+								<form role="form" id="payment-form" method="POST" action="#">
+									<div class="col-md-8">
+										<div class="form-group">
+											<div class="col-md-12">
+												<label for="cardNumber">Card number</label>
+												<div class="input-group">
+													<input type="tel" class="form-control" name="cardNumber"
+														placeholder="Enter Card Number"
+													/>
+													<span class="input-group-addon"><i class="fa fa-credit-card"></i></span>
+												</div>
+											</div>
+										</div>                            
+										<div class="form-group card-exp">
+											<div class="col-md-6">
+												<label for="cardExpiry">Expiry Date</label>
+												<input type="tel" class="form-control" name="cardExpiry" placeholder="MM"/>
+												/ <input type="tel" class="form-control" name="cardExpiry" placeholder="YY"/>
+											</div>
+											<div class="col-md-6">
+												<label for="cardCVC" style="position: relative;margin-left: auto;margin-right: 2em;">CVV</label>
+												<input type="tel" class="form-control pull-right" name="cardCVC" />
+											</div>
+										</div>
+										<div class="col-md-12">
+											<div class="pay-info">
+												<p>Your card will save for future usage upon BID Confirmation</p>
+												<p>Bid Participation Charges  <b>(1$ Non Refundable)</b></p>
+											</div>
+										</div>
+									</div>
+									<div class="col-md-4 actual-cash">
+										<p>Total Cash to be Paid <b><i class="fa fa-dollar"></i>1200.00</b></p>
+										<a href="#" type="button" class="btn btn-danger">Pay Now</a>
+									</div>
+								</form>
+							</div>
+						</div>
+						<div class="col-md-3">
+							<div class="side-video">
+								<iframe src="https://www.youtube.com/embed/_O2_nTt1N6w" width="100%" height="180"></iframe>
+							</div>
+							<div class="side-image">
+								<img class="img-responsive" src="<?php echo base_url('assets/home/images/multi-bid/mb2.jpg'); ?>" alt="img1">
+							</div>
+							<div class="side-image">
+								<img class="img-responsive" src="<?php echo base_url('assets/home/images/multi-bid/mb3.jpg'); ?>" alt="img1">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
