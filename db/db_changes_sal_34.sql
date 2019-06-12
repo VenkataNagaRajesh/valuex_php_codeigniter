65475940fae425a0616da4f35796139739f29f24
salini.gunturu@sweken.com <salini.gunturu@sweken.com>
2019-06-12 15:28:04 +0530

    adding columns for bid values

---
+++ db/db_changes_sal_34.sql
@@ -0,0 +1,4 @@
+ALTER TABLE VX_aln_dtpf_ext ADD COLUMN cash int(11) after fclr_id;
+ALTER TABLE VX_aln_dtpf_ext ADD COLUMN miles int(11) after fclr_id;
+ALTER TABLE VX_aln_dtpf_ext ADD COLUMN bid_submit_date  int(11) after fclr_id;
+
