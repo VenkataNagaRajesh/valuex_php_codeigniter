665def12b3d0b3f420192e4a5c067bbaf7e4c3ed
Anitha Jeereddi <anitha.jeereddi@sweken.com>
2019-06-14 17:09:14 +0530

    feedback and bid error screens added

--- mvc/views/_layout_home.php
+++ mvc/views/_layout_home.php
@@ -12,14 +12,10 @@
 	<link href="<?php echo base_url('assets/home/css/animate.css'); ?>" rel="stylesheet">
 	<link href="<?php echo base_url('assets/home/css/icomoon.css'); ?>" rel="stylesheet">
 	<link href="<?php echo base_url('assets/home/css/bootstrap.css'); ?>" rel="stylesheet">
-	<link href="<?php echo base_url('assets/home/css/superfish.css'); ?>" rel="stylesheet">
 	<link href="<?php echo base_url('assets/home/css/magnific-popup.css'); ?>" rel="stylesheet">
 	<link href="<?php echo base_url('assets/home/css/bootstrap-datepicker.min.css'); ?>" rel="stylesheet">
-	<link href="<?php echo base_url('assets/home/css/cs-select.css'); ?>" rel="stylesheet">
-	<link href="<?php echo base_url('assets/home/css/cs-skin-border.css'); ?>" rel="stylesheet">
 	<link href="<?php echo base_url('assets/home/css/style.css'); ?>" rel="stylesheet">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">	
 	 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/10.6.1/bootstrap-slider.js"></script>
@@ -32,28 +28,12 @@
 <body>	
 
 	<!--<script type="text/javascript" src="<?php echo base_url('assets/home/js/jquery.min.js');?>"></script>-->	
-
-
-		<script type="text/javascript" src="<?php echo base_url('assets/home/js/modernizr-2.6.2.min.js');?>"></script>
-	<script type="text/javascript" src="<?php echo base_url('assets/home/js/jquery.easing.1.3.js');?>"></script>	
-	<script type="text/javascript" src="<?php echo base_url('assets/home/js/bootstrap.min.js');?>"></script>	
-	<script type="text/javascript" src="<?php echo base_url('assets/home/js/jquery.waypoints.min.js');?>"></script>
-	<script type="text/javascript" src="<?php echo base_url('assets/home/js/sticky.js');?>"></script>	
-	<script type="text/javascript" src="<?php echo base_url('assets/home/js/jquery.stellar.min.js');?>"></script>
-	<script type="text/javascript" src="<?php echo base_url('assets/home/js/hoverIntent.js');?>"></script>
-	<script type="text/javascript" src="<?php echo base_url('assets/home/js/superfish.js');?>"></script>	
-	<script type="text/javascript" src="<?php echo base_url('assets/home/js/jquery.magnific-popup.min.js');?>"></script>
-	<script type="text/javascript" src="<?php echo base_url('assets/home/js/magnific-popup-options.js');?>"></script>	
+	<script type="text/javascript" src="<?php echo base_url('assets/home/js/bootstrap.min.js');?>"></script>		
 	<script type="text/javascript" src="<?php echo base_url('assets/home/js/bootstrap-datepicker.min.js');?>"></script>
-	<script type="text/javascript" src="<?php echo base_url('assets/home/js/classie.js');?>"></script>
-	<script type="text/javascript" src="<?php echo base_url('assets/home/js/selectFx.js');?>"></script>
     <script type="text/javascript" src="<?php echo base_url('assets/home/js/main.js');?>"></script>
 	<script type="text/javascript" src="<?php echo base_url('assets/home/js/jquery.smartWizard.min.js');?>"></script>
-	<!--<script type="text/javascript" src="<?php echo base_url('assets/home/js/colored.slider.js');?>"></script>-->
-	<!--<script src='https://www.google.com/recaptcha/api.js'></script>
-<link rel="stylesheet" type="text/css" href="http://code.jquery.com/mobile/1.4.1/jquery.mobile-1.4.1.min.css">
-<script type='text/javascript' src="http://code.jquery.com/mobile/1.4.1/jquery.mobile-1.4.1.min.js"></script>
-<script type="text/javascript" src="http://localhost/valuex/assets/home/js/colored.slider.js"></script>-->
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script type="text/javascript" src="<?php echo base_url('assets/home/js/simple-rating.js');?>"></script>
 
 <?php $this->load->view($subview); ?>
 
---
+++ mvc/views/home/feedback.php
@@ -0,0 +1,92 @@
+<div class="container top-bar">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="col-md-2">
+				<img class="img-responsive" src="<?php echo base_url('assets/home/images/emir.png'); ?>" alt="logo">
+			</div>
+		</div>
+	</div>
+</div>
+<div class="container">
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<div class="feedback-widget">
+				<div class="feedback-title">
+					<h2>Feedback Widget</h2>
+					<p><b>Give us your Valuable feedback - we are listening!</b></p>
+				</div>
+				<div class="feedback-bar">
+					<h3>
+						Hi Ram !!
+						<span class="pull-right">Connect with us 
+							<a href="#"><i class="fa fa-facebook"></i></a>
+							<a href="#"><i class="fa fa-twitter"></i></a>
+							<a href="#"><i class="fa fa-youtube"></i></a>
+						</span>
+					</h3>
+					<form>
+						<p>1.Overall Experience</p>
+							<label class="radio-inline">
+							  <input type="radio" name="optradio" checked>Very Good
+							</label>
+							<label class="radio-inline">
+							  <input type="radio" name="optradio">Good
+							</label>
+							<label class="radio-inline">
+							  <input type="radio" name="optradio">Good Fair
+							</label>
+							<label class="radio-inline">
+							  <input type="radio" name="optradio">Poor
+							</label>
+						<p>2. Timely Response</p>
+							<label class="radio-inline">
+							  <input type="radio" name="optradio" checked>Very Good
+							</label>
+							<label class="radio-inline">
+							  <input type="radio" name="optradio">Good
+							</label>
+							<label class="radio-inline">
+							  <input type="radio" name="optradio">Good Fair
+							</label>
+							<label class="radio-inline">
+							  <input type="radio" name="optradio">Poor
+							</label>
+						<p>3. Our Support</p>
+							<label class="radio-inline">
+							  <input type="radio" name="optradio" checked>Very Good
+							</label>
+							<label class="radio-inline">
+							  <input type="radio" name="optradio">Good
+							</label>
+							<label class="radio-inline">
+							  <input type="radio" name="optradio">Good Fair
+							</label>
+							<label class="radio-inline">
+							  <input type="radio" name="optradio">Poor
+							</label>
+						<p>4. Overall Satisfaction</p>
+							<label class="radio-inline">
+							  <input type="radio" name="optradio" checked>Very Good
+							</label>
+							<label class="radio-inline">
+							  <input type="radio" name="optradio">Good
+							</label>
+							<label class="radio-inline">
+							  <input type="radio" name="optradio">Good Fair
+							</label>
+							<label class="radio-inline">
+							  <input type="radio" name="optradio">Poor
+							</label>
+						<p class="want-rate">Want to rate with us for customer services?</p>
+						<div class="feed-rating">
+							<input class="rating">
+						</div>
+						<p>Is there anything you would like to tell us?</p>
+						<textarea col="30" rows="2" class="form-control" placeholder="Type Here"></textarea>
+						<button type="button" class="btn btn-danger col-md-offset-3">Submit</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
