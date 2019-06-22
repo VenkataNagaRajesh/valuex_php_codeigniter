35f20c1791d14e0105963be392383af896308749
Lakshmi <lakshmi.amujuru@sweken.com>
2019-06-22 13:45:26 +0530

    mailandsms templates db changes

---
+++ db/db_changes_lak_13.sql
@@ -0,0 +1,15 @@
+alter table mailandsmstemplatetag modify column create_date int(11) DEFAULT 0;
+alter table mailandsmstemplate add column catID int(11) NOT NULL;
+alter table mailandsmstemplate add column `default` tinyint(1) DEFAULT 0 ;
+insert into mailandsmstemplatetag set usertypeID = 12,create_date=1560342432,tagname='[first_name]';
+insert into mailandsmstemplatetag set usertypeID = 12,create_date=1560342432,tagname='[last_name]';
+insert into mailandsmstemplatetag set usertypeID = 12,create_date=1560342432,tagname='[pnr_ref]';
+
+CREATE TABLE `mailandsmscategory` (
+  `catID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) NOT NULL,
+  `alias` varchar(128) NOT NULL,  
+  PRIMARY KEY (`catID`)
+);
+
+insert into mailandsmscategory set name='Offer Mail',alias='offer_mail';
