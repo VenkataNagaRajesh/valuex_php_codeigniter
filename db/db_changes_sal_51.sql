7bea63be351cdc54da0fa32089123c6c3b70eeef
salini.gunturu@sweken.com <salini.gunturu@sweken.com>
2019-07-17 10:34:01 +0530

    fixing issue

--- db/db_changes_sal_51.sql
+++ db/db_changes_sal_51.sql
@@ -1,3 +1,3 @@
 ALTER TABLE VX_aln_daily_tkt_pax_feed ADD COLUMN frequency int(11) AFTER dep_date;
-ALTER TABLE VX_aln_daily_tkt_pax_feed ADD COLUMN tier int(11) AFTER dep_date NOT NULL default 0;
+ALTER TABLE VX_aln_daily_tkt_pax_feed ADD COLUMN tier int(11) NOT NULL default 0 AFTER dep_date;
 
