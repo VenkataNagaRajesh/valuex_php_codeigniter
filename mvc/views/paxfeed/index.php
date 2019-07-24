2c045b48d26ca970cc5f047ed94ff8868b54771a
salini.gunturu@sweken.com <salini.gunturu@sweken.com>
2019-07-24 23:09:39 +0530

    adding bulk delete for pax

--- mvc/views/paxfeed/index.php
+++ mvc/views/paxfeed/index.php
@@ -142,11 +142,14 @@
                 </div>	             				
 			  </div>
 			 </form>			
-            <div id="hide-table">
-               <table id="paxfeedtable" class="table table-striped table-bordered table-hover dataTable no-footer">
+
+<div class="col-md-12">
+                <div class="col-md-12">
+                        <div class="tab-content table-responsive" id="hide-table">
+               <table id="paxfeedtable" class="table table-bordered dataTable no-footer">
                  <thead>
                     <tr>
-                        <th class="col-lg-1"><?=$this->lang->line('slno')?></th>
+			 <th><input type="checkbox" id="bulkDelete"/> <button id="deleteTriger">Delete All</button></th>
                         <th class="col-lg-2"><?=$this->lang->line('airline_code')?></th>
 			<th class="col-lg-1"><?=$this->lang->line('pnr_ref')?></th>
 			<th class="col-lg-1"><?=$this->lang->line('pax_nbr')?></th>
@@ -186,6 +189,8 @@
                  </tbody>
               </table>
             </div>
+			</div>
+		</div>
           </div>
        </div>
    </div>
@@ -219,7 +224,7 @@
                     "success": fnCallback
                          } ); }, 
 
-      "columns": [{"data": "dtpf_id" },
+      "columns": [{"data": "chkbox" },
                   {"data": "airline_code" },
 				  {"data": "pnr_ref" },
 				  {"data": "pax_nbr"},
@@ -257,7 +262,9 @@
 				{ extend: 'csv', exportOptions: { columns: "thead th:not(.noExport)" } },
 				{ extend: 'excel', exportOptions: { columns: "thead th:not(.noExport)" } },
 				{ extend: 'pdf', exportOptions: { columns: "thead th:not(.noExport)" } }                
-            ] 	
+            ] 	,
+
+	"columnDefs": [ {"targets": 0,"orderable": false,"searchable": false,"width": "130px" }]
     });
 	
 	
@@ -336,4 +343,49 @@
 $( ".select2" ).select2();
 $('#start_date').datepicker();
 $('#end_date').datepicker();
+
+$(document).ready(function() { 
+
+$("#bulkDelete").on('click',function() { // bulk checked
+        var status = this.checked;
+        $(".deleteRow").each( function() {
+          if(status == 1 && $(this).prop('checked')) {
+                
+          } else {
+            $(this).prop("checked",status);
+            $(this).not("#bulkDelete").closest('tr').toggleClass('rowselected');
+         }
+        });
+    });
+
+
+
+$('#deleteTriger').on("click", function(event){ // triggering delete one by one
+        if( $('.deleteRow:checked').length > 0 ){  // at-least one checkbox checked
+            var ids = [];
+            $('.deleteRow').each(function(){
+                if($(this).is(':checked')) { 
+                    ids.push($(this).val());
+                }
+            });
+            var ids_string = ids.toString();  // array to string conversion 
+            $.ajax({
+                type: "POST",
+                url: "<?php echo base_url('paxfeed/delete_pax_bulk_records'); ?>",
+                data: {data_ids:ids_string},
+                success: function(result) {
+                   $('#paxfeedtable').DataTable().ajax.reload();
+                },
+                async:false
+            });
+        }
+    }); 
+
+
+$('#paxfeedtable').on('click', 'input[type="checkbox"]', function() {
+        $(this).not("#bulkDelete").parents("tr").toggleClass('rowselected');
+    });
+
+
+});
  </script>
