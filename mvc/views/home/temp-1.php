26213f3593b6b22fbf5d520e6562b29e08530fed
Anitha Jeereddi <anitha.jeereddi@sweken.com>
2019-08-24 16:51:07 +0530

    temp1 php template

---
+++ mvc/views/home/temp-1.php
@@ -0,0 +1,69 @@
+<style>
+	@media (min-width: 320px) and (max-width: 600px) {
+		.mobile{
+			width:100% !important;
+		}
+		.mobile img{
+			height:auto !important;
+		}
+		.mobile h1{
+			font-size:14px !important;
+		}
+	}
+</style>
+<div style="margin-left:auto;margin-right:auto;width:100%;">
+	<div class="mobile" style="margin-left:auto;margin-right:auto;width:50%;padding-left: 15px;padding-right: 15px;background: #333;">
+		<img style="width:100px;" src="<?=base_url()?>assets/home/images/emir.png" alt="logo">
+	</div>
+	<div class="mobile" style="border: solid 1px #ddd;width:50%;margin-right: auto;margin-left: auto;padding-left: 15px;padding-right: 15px;">
+		<div style="position: relative;min-height: 1px;">
+			<div style="background-image: url(../assets/home/images/temp1-hdr-bg.jpg);margin-top: 15px;">
+				<h1 style="margin-bottom:8px;color: #fff;font-size: 24px;font-weight: bold;padding:12px;font-family:calibri;"><span>Hi!</span><span style="float: right !important;color:#333;">Booking Reference : <b></b></span></h1>
+			</div>
+			<div>
+				<img style="width: 100%;display: block;max-width: 100%;height: 150px;" src="<?=base_url()?>assets/home/images/temp3-bnr.jpg" alt="temp bnr">
+			</div>
+		</div>
+		<div style="font-family:calibri;font-weight:500;position: relative;min-height: 1px;margin-top:10px;margin-bottom:10px;overflow: auto;">
+			<table style="border-collapse: collapse;border-spacing: 0;">
+				<thead style="background: #ff6633;color: #fff;text-transform: capitalize;">
+					<tr>
+						<th style="padding: 0 3px;text-align:center;font-size:14px;">Date</th>
+						<th style="padding: 0 3px;text-align:center;font-size:14px;">Flight</th>
+						<th style="padding: 0 3px;text-align:center;font-size:14px;">Time</th>
+						<th style="padding: 0 3px;text-align:center;font-size:14px;">Origin</th>
+						<th style="padding: 0 3px;text-align:center;font-size:14px;">Destination</th>
+						<th style="padding: 0 3px;text-align:center;font-size:14px;">Cabin no</th>
+						<th style="padding: 0 3px;text-align:center;font-size:14px;">Seat No</th>
+						<th style="padding: 0 3px;text-align:center;font-size:14px;">Offer</th>
+					</tr>
+				</thead>
+			  <tbody>
+					<tr>
+						<td style="padding: 0 3px;text-align:center;font-size:14px;color: #333;background: #f5f5f5;">{dep_date}</td>
+						<td style="padding: 0 3px;text-align:center;font-size:14px;color: #333;background: #f5f5f5;">{carrier_code}{flight_number}</td>
+						<td style="padding: 0 3px;text-align:center;font-size:14px;color: #333;background: #f5f5f5;">{dep_time}</td>
+						<td style="padding: 0 3px;text-align:center;font-size:14px;color: #333;background: #f5f5f5;">{from_city}</td>
+						<td style="padding: 0 3px;text-align:center;font-size:14px;color: #333;background: #f5f5f5;">{to_city}</td>
+						<td style="padding: 0 3px;text-align:center;font-size:14px;color: #333;background: #f5f5f5;">{cabin} Class</td>
+						<td style="padding: 0 3px;text-align:center;font-size:14px;color: #333;background: #f5f5f5;">{seat_no}</td>
+						<td style="padding: 0 3px;text-align:center;font-size:14px;color: #333;background: #f5f5f5;">${cash} USD</td>
+					</tr>
+				</tbody>
+			</table>
+			</div>
+		<div style="position: relative;min-height: 1px;padding-left: 15px;padding-right:15px;">
+			<p style="margin-bottom: 10px;font-size: 14px;color: #333;font-family:calibri;font-weight:500;text-align:center;">
+				With AC Bid Upgrade you can enhance your travel experince by enjoying priority. One or more of yourupcoming flight (s) are eligible for bids
+			</p>
+			<p style="text-align:center;"><a type="button" href="#" style="background: #ff6633;font-weight:bold;padding:8px 26px;font-size: 23px;border-radius: 33px;border:none;color:#fff;font-family:calibri;text-decoration:none;">BID NOW</a></p>
+			<div class="info">
+				<p style="margin-bottom: 10px;font-weight:500;font-size: 14px;color: #333;font-family: calibri;text-align:center;">
+					<span><b>Help Center | I'm Not Intrested</b></span><br>
+					<span>Value X S.r.l. via Paleocapa, 7 - 20121 Milan - Italy - Tax code and VAT number: IT11876271005</span><br>
+					You are receiving this email because you subscribed to the Travel Alert service, accepting Value X-  Terms of Service and Privacy Policy .
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
