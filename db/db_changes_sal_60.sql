10c617afd6d1d8d9d8c4c5dd84e1de11b76b02f0
salini.gunturu@sweken.com <salini.gunturu@sweken.com>
2019-08-27 15:14:45 +0530

    adding season accurate changes

---
+++ db/db_changes_sal_60.sql
@@ -0,0 +1,14 @@
+insert into vx_aln_data_types set name='level_order', alias='Level Order', create_userID=1,modify_userID=1,active=1,create_date=1554788423,modify_date=1554788423;
+
+insert into vx_aln_data_defns (aln_data_typeID,aln_data_value,alias,code,create_userID,modify_userID,create_date,modify_date) values ('23','Airport','1','1','1','1','1554788423','1554788423');
+
+insert into vx_aln_data_defns (aln_data_typeID,aln_data_value,alias,code,create_userID,modify_userID,create_date,modify_date) values ('23','City','3','2','1','1','1554788423','1554788423');
+
+insert into vx_aln_data_defns (aln_data_typeID,aln_data_value,alias,code,create_userID,modify_userID,create_date,modify_date) values ('23','Country','2','3','1','1','1554788423','1554788423');
+
+insert into vx_aln_data_defns (aln_data_typeID,aln_data_value,alias,code,create_userID,modify_userID,create_date,modify_date) values ('23','Region','4','4','1','1','1554788423','1554788423');
+
+insert into vx_aln_data_defns (aln_data_typeID,aln_data_value,alias,code,create_userID,modify_userID,create_date,modify_date) values ('23','Area','5','5','1','1','1554788423','1554788423');
+
+insert into vx_aln_data_defns (aln_data_typeID,aln_data_value,alias,code,create_userID,modify_userID,create_date,modify_date) values ('23','MarketZone','17','6','1','1','1554788423','1554788423');
+
