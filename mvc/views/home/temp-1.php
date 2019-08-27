08508068be9d94d66016bf3b0f86106f34bbd999
Lakshmi <lakshmi.amujuru@sweken.com>
2019-08-27 17:12:18 +0530

    mail template editor(CKEditor) integration

--- mvc/views/home/temp-1.php
+++ mvc/views/home/temp-1.php
@@ -1,53 +1,41 @@
-<style>
-	@media (min-width: 320px) and (max-width: 600px) {
-		.mobile{
-			width:100% !important;
-		}
-		.mobile img{
-			height:auto !important;
-		}
-		.mobile h1{
-			font-size:14px !important;
-		}
-	}
-</style>
-<div style="margin-left:auto;margin-right:auto;width:100%;">
-	<div class="mobile" style="margin-left:auto;margin-right:auto;width:50%;padding-left: 15px;padding-right: 15px;background: #333;">
-		<img style="width:100px;" src="<?=base_url()?>assets/home/images/emir.png" alt="logo">
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<div style="min-width: 245px;max-width: 1920px;position: relative;padding: 10px;border: 1px dotted #ddd;">
+	<div style="margin-left:auto;margin-right:auto;width:100%;padding-left: 15px;padding-right: 15px;background: #333;">
+		<img style="width:120px;" src="<?=base_url()?>assets/home/images/emir.png" alt="logo">
 	</div>
-	<div class="mobile" style="border: solid 1px #ddd;width:50%;margin-right: auto;margin-left: auto;padding-left: 15px;padding-right: 15px;">
+	<div style="border: solid 1px #ddd;width:100%;margin-right: auto;margin-left: auto;padding-left: 15px;padding-right: 15px;">
 		<div style="position: relative;min-height: 1px;">
-			<div style="background-image: url(../assets/home/images/temp1-hdr-bg.jpg);margin-top: 15px;">
-				<h1 style="margin-bottom:8px;color: #fff;font-size: 24px;font-weight: bold;padding:12px;font-family:calibri;"><span>Hi!</span><span style="float: right !important;color:#333;">Booking Reference : <b></b></span></h1>
+			<div style="background-image: url(../assets/home/images/temp1-hdr-bg.jpg);margin-top: 15px;background-repeat: no-repeat;width: 100%;background-size: cover;">
+				<h1 style="margin-bottom:8px;color: #fff;font-size: 24px;font-weight: bold;padding:12px;font-family:calibri;"><span>Hi PAX Name! </span><span style="float: right !important;color:#333;">Booking Reference : PNR Ref<b></b></span></h1>
 			</div>
 			<div>
-				<img style="width: 100%;display: block;max-width: 100%;height: 150px;" src="<?=base_url()?>assets/home/images/temp3-bnr.jpg" alt="temp bnr">
+				<img style="width: 100%;display: block;max-width: 100%;height: auto;" src="<?=base_url()?>assets/home/images/temp3-bnr.jpg" alt="temp bnr">
 			</div>
 		</div>
-		<div style="font-family:calibri;font-weight:500;position: relative;min-height: 1px;margin-top:10px;margin-bottom:10px;overflow: auto;">
-			<table style="border-collapse: collapse;border-spacing: 0;">
+		<div style="font-family:calibri;font-weight:500;position: relative;min-height: 1px;margin-top:10px;margin-bottom:10px;overflow: auto;width:100%;">
+			<table style="border-collapse: collapse;border-spacing: 0;width:100%;">
 				<thead style="background: #ff6633;color: #fff;text-transform: capitalize;">
-					<tr>
-						<th style="padding: 0 3px;text-align:center;font-size:14px;">Date</th>
-						<th style="padding: 0 3px;text-align:center;font-size:14px;">Flight</th>
-						<th style="padding: 0 3px;text-align:center;font-size:14px;">Time</th>
-						<th style="padding: 0 3px;text-align:center;font-size:14px;">Origin</th>
-						<th style="padding: 0 3px;text-align:center;font-size:14px;">Destination</th>
-						<th style="padding: 0 3px;text-align:center;font-size:14px;">Cabin no</th>
-						<th style="padding: 0 3px;text-align:center;font-size:14px;">Seat No</th>
-						<th style="padding: 0 3px;text-align:center;font-size:14px;">Offer</th>
+					<tr style="white-space:nowrap;">
+						<th style="padding: 6px 3px;text-align:center;font-size:14px;">Date</th>
+						<th style="padding: 6px 3px;text-align:center;font-size:14px;">Flight</th>
+						<th style="padding: 6px 3px;text-align:center;font-size:14px;">Time</th>
+						<th style="padding: 6px 3px;text-align:center;font-size:14px;">Origin</th>
+						<th style="padding: 6px 3px;text-align:center;font-size:14px;">Destination</th>
+						<th style="padding: 6px 3px;text-align:center;font-size:14px;">Cabin no</th>
+						<th style="padding: 6px 3px;text-align:center;font-size:14px;">Seat No</th>
+						<th style="padding: 6px 3px;text-align:center;font-size:14px;">Offer</th>
 					</tr>
 				</thead>
 			  <tbody>
-					<tr>
-						<td style="padding: 0 3px;text-align:center;font-size:14px;color: #333;background: #f5f5f5;">{dep_date}</td>
-						<td style="padding: 0 3px;text-align:center;font-size:14px;color: #333;background: #f5f5f5;">{carrier_code}{flight_number}</td>
-						<td style="padding: 0 3px;text-align:center;font-size:14px;color: #333;background: #f5f5f5;">{dep_time}</td>
-						<td style="padding: 0 3px;text-align:center;font-size:14px;color: #333;background: #f5f5f5;">{from_city}</td>
-						<td style="padding: 0 3px;text-align:center;font-size:14px;color: #333;background: #f5f5f5;">{to_city}</td>
-						<td style="padding: 0 3px;text-align:center;font-size:14px;color: #333;background: #f5f5f5;">{cabin} Class</td>
-						<td style="padding: 0 3px;text-align:center;font-size:14px;color: #333;background: #f5f5f5;">{seat_no}</td>
-						<td style="padding: 0 3px;text-align:center;font-size:14px;color: #333;background: #f5f5f5;">${cash} USD</td>
+					<tr style="white-space:nowrap;">
+						<td style="padding: 6px 3px;text-align:center;font-size:14px;color: #333;background: #f5f5f5;">{dep_date}</td>
+						<td style="padding: 6px 3px;text-align:center;font-size:14px;color: #333;background: #f5f5f5;">{carrier_code}{flight_number}</td>
+						<td style="padding: 6px 3px;text-align:center;font-size:14px;color: #333;background: #f5f5f5;">{dep_time}</td>
+						<td style="padding: 6px 3px;text-align:center;font-size:14px;color: #333;background: #f5f5f5;">{from_city}</td>
+						<td style="padding: 6px 3px;text-align:center;font-size:14px;color: #333;background: #f5f5f5;">{to_city}</td>
+						<td style="padding: 6px 3px;text-align:center;font-size:14px;color: #333;background: #f5f5f5;">{cabin} Class</td>
+						<td style="padding: 6px 3px;text-align:center;font-size:14px;color: #333;background: #f5f5f5;">{seat_no}</td>
+						<td style="padding: 6px 3px;text-align:center;font-size:14px;color: #333;background: #f5f5f5;">${cash} USD</td>
 					</tr>
 				</tbody>
 			</table>
