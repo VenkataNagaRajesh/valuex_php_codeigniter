2b0fde4528c2ebdcbfd4219f2fcc9a581b789ac0
Anitha Jeereddi <anitha.jeereddi@sweken.com>
2019-09-17 16:43:35 +0530

    delete button added in airline images

--- mvc/views/airline/view.php
+++ mvc/views/airline/view.php
@@ -45,83 +45,98 @@
 				<iframe src="<?=str_replace('watch?v=','embed/',$airline->video_links)?>" width="100%" height="130"></iframe>
 			</div>
         </div>
-		<div class="col-md-12 airline-cabin-img" style="padding:0">
-		  <h2>Gallery</h2>
-             		  
-		  <?php
-             foreach($airline->gallery as $image){
- 		         $array = array("src" => base_url('uploads/images/'.$image->image),
-                                'width' => '150px',
-                                'height' => '150px',
-                                'class' => 'img-rounded'
-                          );
-				echo img($array);
-			 }
-            ?>
-						
+		<div class="adm-brands">
+			<div class="col-md-12 airline-cabin-img" style="padding:0">
+				<h2>Gallery</h2>				
+				  <?php $i=1;
+					 foreach($airline->gallery as $image){ ?>
+					 <div class="col-md-2" id="al-gallery-<?=$i?>">
+						 <div class="img-box"> 
+							<?php $array = array("src" => base_url('uploads/images/'.$image->image),
+											'width' => '150px',
+											'height' => '150px',
+											'class' => 'img-rounded image'
+									  );
+							echo img($array); ?>
+							<div class="middle">
+								<button type="button" class="btn btn-danger btn-sm btn-del" id="del-btn-<?=$i?>"><i class="fa fa-trash"></i></button>
+							</div>
+						</div>
+					</div>
+					<?php $i++; }?>
+				</div>
+			</div>
+			<div class="col-md-12 airline-cabin-img" style="padding:0">
+			  <h2>Upgrade Offer Mail Template 1</h2>
+						  
+			  <?php
+				 foreach($airline->upgrade_offer_mail_template1 as $image){
+					 $array = array("src" => base_url('uploads/images/'.$image->image),
+									'width' => '150px',
+									'height' => '150px',
+									'class' => 'img-rounded'
+							  );
+					echo img($array);
+				 }
+				?>
+							
+			</div>
+			<div class="col-md-12 airline-cabin-img" style="padding:0">
+			  <h2>Upgrade Offer Mail Template 2</h2>
+						  
+			  <?php
+				 foreach($airline->upgrade_offer_mail_template2 as $image){
+					 $array = array("src" => base_url('uploads/images/'.$image->image),
+									'width' => '150px',
+									'height' => '150px',
+									'class' => 'img-rounded'
+							  );
+					echo img($array);
+				 }
+				?>
+							
+			</div>
+			<div class="col-md-12 airline-cabin-img" style="padding:0">
+			  <h2>Upgrade Offer Mail Template 3</h2>
+						  
+			  <?php
+				 foreach($airline->upgrade_offer_mail_template3 as $image){
+					 $array = array("src" => base_url('uploads/images/'.$image->image),
+									'width' => '150px',
+									'height' => '150px',
+									'class' => 'img-rounded'
+							  );
+					echo img($array);
+				 }
+				?>
+							
+			</div>
+			<div class="col-md-12 airline-cabin-img" style="padding:0">
+			  <h2>Airline Logo for Mail Template(84*60)</h2>
+						  
+			  <?php
+				 foreach($airline->airline_logo as $image){
+					 $array = array("src" => base_url('uploads/images/'.$image->image),
+									'width' => '150px',
+									'height' => '150px',
+									'class' => 'img-rounded'
+							  );
+					echo img($array);
+				 }
+				?>
+							
+			</div>
 		</div>
-		<div class="col-md-12 airline-cabin-img" style="padding:0">
-		  <h2>Upgrade Offer Mail Template 1</h2>
-             		  
-		  <?php
-             foreach($airline->upgrade_offer_mail_template1 as $image){
- 		         $array = array("src" => base_url('uploads/images/'.$image->image),
-                                'width' => '150px',
-                                'height' => '150px',
-                                'class' => 'img-rounded'
-                          );
-				echo img($array);
-			 }
-            ?>
-						
-		</div>
-		<div class="col-md-12 airline-cabin-img" style="padding:0">
-		  <h2>Upgrade Offer Mail Template 2</h2>
-             		  
-		  <?php
-             foreach($airline->upgrade_offer_mail_template2 as $image){
- 		         $array = array("src" => base_url('uploads/images/'.$image->image),
-                                'width' => '150px',
-                                'height' => '150px',
-                                'class' => 'img-rounded'
-                          );
-				echo img($array);
-			 }
-            ?>
-						
-		</div>
-		<div class="col-md-12 airline-cabin-img" style="padding:0">
-		  <h2>Upgrade Offer Mail Template 3</h2>
-             		  
-		  <?php
-             foreach($airline->upgrade_offer_mail_template3 as $image){
- 		         $array = array("src" => base_url('uploads/images/'.$image->image),
-                                'width' => '150px',
-                                'height' => '150px',
-                                'class' => 'img-rounded'
-                          );
-				echo img($array);
-			 }
-            ?>
-						
-		</div>
-		<div class="col-md-12 airline-cabin-img" style="padding:0">
-		  <h2>Airline Logo for Mail Template(84*60)</h2>
-             		  
-		  <?php
-             foreach($airline->airline_logo as $image){
- 		         $array = array("src" => base_url('uploads/images/'.$image->image),
-                                'width' => '150px',
-                                'height' => '150px',
-                                'class' => 'img-rounded'
-                          );
-				echo img($array);
-			 }
-            ?>
-						
-		</div>
-		</div>
-  </section>
+	</div>
+ </section>
+  <script>
+  <?php $i=1;
+	 foreach($airline->gallery as $image){ ?>
+		$("#del-btn-"+<?=$i?>).click(function(){
+			$("#al-gallery-"+<?=$i?>).remove();
+		});
+	 <?php } ?>
+	</script>
 </div>
   
  
