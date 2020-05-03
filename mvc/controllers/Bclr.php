0f8212c1bc958a4e13acb1acf14ce747098c017f
Dileep Nadukuru <dileep.nadukuru@sweken.com>
2020-05-03 19:30:15 +0530

    add condition in bclr by Dileep N

--- mvc/controllers/Bclr.php
+++ mvc/controllers/Bclr.php
@@ -898,7 +898,7 @@ class Bclr extends Admin_Controller
         // print_r($rResult);
         foreach ($rResult as $feed) {
             $feed->bag_type_value = ($feed->bag_type == 1) ? "KG" : "Piece";
-            $feed->from_cabin_value = ($feed->from_cabin_value == "") ? "*" : $feed->from_cabin_value;
+            $feed->from_cabin_value = ($feed->from_cabin_value == "") ? "All(*)" : $feed->from_cabin_value;
             $feed->allowance = ($feed->allowance == 1) ? "Whitelist" : "Blacklist";
             $feed->dep_time_start = gmdate("H:i:s", $feed->dep_time_start);
             $feed->dep_time_end = gmdate("H:i:s", $feed->dep_time_end);
