3118cdbbbfb31563acafa06aad00c9c33d75b2d8
vamsig <vamsi63@gmail.com>
2020-10-04 11:37:13 -0600

    added advanced bid report for any product types

--- mvc/controllers/Adreport.php
+++ mvc/controllers/Adreport.php
@@ -7,6 +7,7 @@ class Adreport extends Admin_Controller {
 		$this->load->model('report_m');
 		$this->load->model('reportdata_m');
 		$this->load->model('user_m');
+		$this->load->model('product_m');
 		$this->load->model('airline_m');
 		$this->load->model('invfeed_m');
 		$language = $this->session->userdata('lang');
@@ -28,47 +29,132 @@ class Adreport extends Admin_Controller {
 		$roleID = $this->session->userdata('roleID');
 		$userID = $this->session->userdata('loginuserID');
 		
-        
-        
-		$sQuery = "SELECT b.alias,b.create_date FROM VX_offer_info a LEFT JOIN VX_data_defns b ON (b.vx_aln_data_defnsID = a.booking_status) LEFT JOIN UP_bid c ON(c.offer_id = a.dtpfext_id)  WHERE b.alias='bid_accepted'";
-             $rResult = $this->install_m->run_query($sQuery);
-             
-        $bidcount = [];
-        $bidcount = $rResult;
-        $bidsaccept = count($bidcount);
-
-        $ssQuery = "SELECT b.alias,b.create_date,c.cash,c.bid_value FROM VX_offer_info a LEFT JOIN VX_data_defns b ON (b.VX_aln_data_defnsID = a.booking_status) LEFT JOIN UP_bid c ON(c.offer_id = a.dtpfext_id) WHERE b.alias='bid_received'";
-             $rrResult = $this->install_m->run_query($ssQuery);
-             
-        $bidrec = [];
-        $bidrec = $rrResult;
-        $bidsrecevied = count($bidrec);
-		$average = $bidsaccept/$bidsrecevied*100;
-
-		$lineResult = [];
-		$line_chart = "SELECT b.alias as bid_status,b.create_date as date FROM VX_offer_info a LEFT JOIN VX_data_defns b ON (b.vx_aln_data_defnsID = a.booking_status) WHERE b.alias='bid_received'";
-		$lineResult = $this->install_m->run_query($line_chart);
-		
-		$bid = [];
-		$dates = [];
-		
-		foreach($lineResult as $row){
-			
-			$bid['bid_status'] = $row->bid_status;
-			$dates['date'] = $row->date;
-			
-			
+		if($this->input->post('filter_airlineID')){
+			$this->data['airlineID'] = $this->input->post('filter_airlineID');
+		} else if($roleID != 1 && $roleID != 5){
+		    $this->data['airlineID'] = $this->session->userdata('default_airline');
+	    	} else {
+			$this->data['airlineID'] = 0;
 		}
+		if($this->input->post('filter_year_to')){
+			$this->data['to_year'] = $this->input->post('filter_year_to');
+		}
+		if($this->input->post('filter_to_month')){
+			$this->data['to_month'] = $this->input->post('filter_to_month');
+		}
+		if($this->input->post('product_id')){
+			$this->data['product_id'] = $this->input->post('product_id');
+		}
+
+		if ( $this->data['to_year'] && $this->data['to_month'] ) {
+			//Last day of current month.
+			$dateString = $this->data['to_year']. '-' . $this->data['to_month'] . '-01';
+			$lastDateOfMonth = date("t", strtotime($dateString));
+			#$this->data['to_date'] = date("t-m-Y", strtotime($this->data['to_year'].'-'.$this->data['to_month'].'-01'));		 
+			$this->data['to_date'] = mktime(23,59,59,$this->data['to_month'],$lastDateOfMonth,$this->data['to_year']);
+		}
+		
+		  if($roleID != 1){
+			$this->data['airlines'] = $this->user_m->getUserAirlines($userID);	   
+		  } else {
+			  $this->data['airlines'] = $this->airline_m->getAirlinesData();
+		  }
+		
+		$bid_accepted_status =  $this->rafeed_m->getDefIdByTypeAndAlias('bid_accepted','20');
+		$bid_rejected_status =  $this->rafeed_m->getDefIdByTypeAndAlias('bid_reject','20');
+
+		$this->data['products'] = $this->product_m->productName();
+        
+        
+		//Accepted Builds
+		$sQuery = "SELECT count(*) as total_bids, sum(bid.bid_value) as total_revenue from UP_bid bid INNER JOIN VX_offer_info oi ON ( oi.dtpfext_id = bid.dtpfext_id) ";
+		$sQuery .= " INNER JOIN VX_daily_tkt_pax_feed  pf ON ( pf.dtpf_id = oi.dtpf_id) ";
+		$sQuery .= " WHERE bid.bid_id is NOT NULL ";
+		if ( $this->data['product_id'] ) {
+			$sQuery .= " AND bid.productID =  ". $this->data['product_id']; 
+		}
+		$sQuery .= " AND  oi.booking_status = $bid_accepted_status "; 
+		if ( $this->data['to_date'] ) {
+			$sQuery .= " AND  pf.dep_date > " . time() .  " AND pf.dep_date <= " . $this->data['to_date'] ; 
+		} else {
+			$sQuery .= " AND  pf.dep_date > " . time(); 
+		}
+		if ( $this->data['airlineID'] ) {
+			$sQuery .= " AND  pf.carrier_code = " .  $this->data['airlineID'];
+		}
+             	$rResult = $this->install_m->run_query($sQuery);
+		$bid_accepted = $rResult[0]->total_bids;
+		$bid_revenue = $rResult[0]->total_revenue;
+
+		//Received/Total Builds
+		$sQuery = "SELECT count(*) as cnt from UP_bid bid INNER JOIN VX_offer_info oi ON ( oi.dtpfext_id = bid.dtpfext_id) ";
+		$sQuery .= " INNER JOIN VX_daily_tkt_pax_feed  pf ON ( pf.dtpf_id = oi.dtpf_id) ";
+		$sQuery .= " WHERE bid.bid_id is NOT NULL ";
+		if ( $this->data['product_id'] ) {
+			$sQuery .= " AND bid.productID =  ". $this->data['product_id']; 
+		}
+		#$sQuery .= " AND  oi.booking_status = $bid_accepted_status "; 
+		if ( $this->data['to_date'] ) {
+			$sQuery .= " AND  pf.dep_date > " . time() .  " AND pf.dep_date <= " . $this->data['to_date'] ; 
+		} else {
+			$sQuery .= " AND  pf.dep_date > " . time(); 
+		}
+		if ( $this->data['airlineID'] ) {
+			$sQuery .= " AND  pf.carrier_code = " .  $this->data['airlineID'];
+		}
+             	$rResult = $this->install_m->run_query($sQuery);
+		$bid_received = $rResult[0]->cnt;
+
+		$bid_average = $bid_received ? ($bid_accepted*100)/$bid_received : 0;
+
+		//Bid Volume
+		$sQuery = "SELECT count(*) as y, DATE_FORMAT(FROM_UNIXTIME(pf.dep_date),'%M-%Y') AS label from UP_bid bid INNER JOIN VX_offer_info oi ON ( oi.dtpfext_id = bid.dtpfext_id) ";
+		$sQuery .= " INNER JOIN VX_daily_tkt_pax_feed  pf ON ( pf.dtpf_id = oi.dtpf_id) ";
+		$sQuery .= " WHERE bid.bid_id is NOT NULL ";
+		if ( $this->data['product_id'] ) {
+			$sQuery .= " AND bid.productID =  ". $this->data['product_id']; 
+		}
+		if ( $this->data['to_date'] ) {
+			$sQuery .= " AND  pf.dep_date > " . time() .  " AND pf.dep_date <= " . $this->data['to_date'] ; 
+		} else {
+			$sQuery .= " AND  pf.dep_date > " . time(); 
+		}
+		if ( $this->data['airlineID'] ) {
+			$sQuery .= " AND  pf.carrier_code = " .  $this->data['airlineID'];
+		}
+		$sQuery .= " GROUP BY DATE_FORMAT(FROM_UNIXTIME(pf.dep_date),'%M-%Y')";
+             	$rResult = $this->install_m->run_query($sQuery);
+		$bid_volume_data = json_decode(json_encode($rResult), true);
+		
+		//Bid Revenue per month
+		$sQuery = "SELECT SUM(bid.bid_value) as y, DATE_FORMAT(FROM_UNIXTIME(pf.dep_date),'%M-%Y') AS label from UP_bid bid INNER JOIN VX_offer_info oi ON ( oi.dtpfext_id = bid.dtpfext_id) ";
+		$sQuery .= " INNER JOIN VX_daily_tkt_pax_feed  pf ON ( pf.dtpf_id = oi.dtpf_id) ";
+		$sQuery .= " WHERE bid.bid_id is NOT NULL ";
+		if ( $this->data['product_id'] ) {
+			$sQuery .= " AND bid.productID =  ". $this->data['product_id']; 
+		}
+		$sQuery .= " AND  oi.booking_status = $bid_accepted_status "; 
+		if ( $this->data['to_date'] ) {
+			$sQuery .= " AND  pf.dep_date > " . time() .  " AND pf.dep_date <= " . $this->data['to_date'] ; 
+		} else {
+			$sQuery .= " AND  pf.dep_date > " . time(); 
+		}
+		if ( $this->data['airlineID'] ) {
+			$sQuery .= " AND  pf.carrier_code = " .  $this->data['airlineID'];
+		}
+		$sQuery .= " GROUP BY DATE_FORMAT(FROM_UNIXTIME(pf.dep_date),'%M-%Y')";
+             	$rResult = $this->install_m->run_query($sQuery);
+		$bid_revenue_data = json_decode(json_encode($rResult), true);
 	
-		
-		
 		$this->data['bid_details'] = $bid_details;
 		$this->data['date'] = $date;
-		$this->data['bidsaccept'] = $bidsaccept;
-		$this->data['bidsrecevied'] = $bidsrecevied; 
-		$this->data['average'] = $average;     
-        $this->data["subview"] = "adreport/index";
+		$this->data['bid_accepted'] = $bid_accepted;
+		$this->data['bid_received'] = $bid_received; 
+		$this->data['bid_average'] = $bid_average;     
+		$this->data['bid_revenue'] = $bid_revenue;     
+		$this->data['bid_volume_data'] = $bid_volume_data;     
+		$this->data['bid_revenue_data'] = $bid_revenue_data;     
+        	$this->data["subview"] = "adreport/index";
 		$this->load->view('_layout_main', $this->data); 
 	}
-	
-	}
+}
--- mvc/views/adreport/index.php
+++ mvc/views/adreport/index.php
@@ -1,24 +1,48 @@
 <?php 
-$a = array( 
-	array("label"=>"Bid Received", "y"=>$bidsrecevied),
-	array("label"=>"Bid Accepted", "y"=>$bidsaccept)
-);
-
-
-$dataPoints = array(
-	array("y" => 25, "label" => "Sunday"),
-	array("y" => 15, "label" => "Monday"),
-	array("y" => 25, "label" => "Tuesday"),
-	array("y" => 5, "label" => "Wednesday"),
-	array("y" => 10, "label" => "Thursday"),
-	array("y" => 0, "label" => "Friday"),
-	array("y" => 20, "label" => "Saturday")
-);
-
 ?> 
 <div class="off-elg">
 	<h2 class="title-tool-bar">Advanced Bids Report</h2>
+	<div class="col-md-12 off-elg-filter-box">
+	   <form class="form-horizontal" role="form" method="post" enctype="multipart/form-data">
+	     <div class="form-group"><br>	   		
+			<div class="col-md-2">			
+			<?php $list = array("0" => "Select Carrier");               
+               foreach($airlines as $airline){
+				 $list[$airline->vx_aln_data_defnsID] = $airline->code;	
+			   }				
+			   echo form_dropdown("filter_airlineID", $list,set_value("filter_airlineID",$airlineID), "id='filter_airlineID' class='form-control hide-dropdown-icon select2'"); ?>	
+			</div>
+			<div class="col-md-4">
+				<fieldset>
+					<legend>To Date</legend>
+					<div class="col-md-6">
+			<?php $ylist = array("0" => "Select To Year"); 
+			    $current_year = date('Y');              
+               		    for($i=0;$i<10;$i++){
+				 $ylist[$current_year+$i] = $current_year+$i;	
+			   }				
+			   echo form_dropdown("filter_year_to", $ylist,set_value("filter_year_to",$year), "id='filter_year_to' class='form-control hide-dropdown-icon select2'"); ?>	 
+			</div>
+			<div class="col-md-6">
+				<select name="filter_to_month" id="filter_to_month" class="form-control hide-dropdown-icon select2" >  				
+				</select>
+					</div>
+				</fieldset>
+			</div>
+                    <div class="col-sm-2">
+			<?php
+			$products['0'] = 'Product Type';
+                            ksort($products);
+			echo form_dropdown("product_id", $products,set_value("product_id",$product_id), "id='product_id' class='form-control hide-dropdown-icon select2'");    ?>
+			</div>
+			<div class="col-md-2 pull-right">
+				<button type="submit" class="form-control btn btn-danger">Report</button>				
+			</div>
+		  </div>
+	   </form>
+	</div>
 <div class="col-md-12 report-box">
+	<div style="color:#f26522"><h3><strong><bold>Advanced Bid <?=$products[$product_id]?> Report for <?= $airlineID ? $list[$airlineID] : " all Carriers" ;?> <?=$this->data['to_date'] ?  "upto &nbsp;" . date('m-d-Y', $this->data['to_date']) : "" ; ?></bold></strong></h3></div>
 	 <div class="row">		
 		<div class="col-md-3">
 		  <h3><b>Accepted Bids</b></h3>
@@ -34,29 +58,21 @@ $dataPoints = array(
 	 <div class="col-md-12" style="border-left:solid 1px #ddd;text-align:center;">			
 			<div>
 				<div class="col-md-3">
-				<div style="margin-bottom: 4em;" id="progress-<?=$bidsaccept?>"  class="pie-title-center" data-percent="<?=round(($bidsaccept/$bidsrecevied)*100)?>" style="height: 180px; width: 100%;">
-						<b><a onclick="progressReport(<?=$bidsaccept?>?>,<?=$bidsrecevied?>)">
-							<span style="cursor:pointer;" data-toggle="tooltip" data-placement="bottom" title="<?=number_format($bidsaccept)?>" class="pie-value"></span>
-						</a>
-						<p>Accepted Bids</p>
-						</b>
+				<div style="margin-bottom: 4em;" id="progress-<?=$bid_accepted?>"  class="pie-title-center" data-percent="<?=number_format(round($bid_average))?>" style="height: 180px; width: 100%;">
+					<b><a onclick="progressReport(<?=$bid_accepted?>?>,<?=$bid_received?>)">
+					<span style="cursor:pointer;" data-toggle="tooltip" data-placement="bottom" title="<?=number_format($bid_accepted)?>" class="pie-value"></span>
+					</a>
+					<p>Accepted Bids</p>
+					</b>
 					</div>
-
 				</div> 
 
 				<div class="col-md-7">
-				<div id="chartContainer" style="height: 370px; width: 100%;"></div>
-
-
+				<div id="bid-volume-chart" style="height: 370px; width: 100%;"></div>
 				</div>  
 
 				<div class="col-md-2">
-				
-				<?php 
-				$total = $bidsrecevied;
-
-				?>
-				<div class="upgrade-revenue-price-box">$<?=number_format($total)?> </div>
+				<div class="upgrade-revenue-price-box">$<?=number_format($bid_revenue)?> </div>
 
 
 				</div>  
@@ -79,17 +95,21 @@ $dataPoints = array(
 			<div>
 				<div class="col-md-3">
 				
-				<?php $total_bids = $bidsaccept+$bidsrecevied; $icon = 'up'; $color = "#0c9e0c";?>
+				<?php $total_bids = $bid_received; $icon = 'up'; $color = "#0c9e0c";?>
 				
 				<div class="upgrade-revenue-price-box" ><?=number_format($total_bids)?> </div>
 				
 				</div>    
 			</div>
-			<div class="col-md-7">
-				</div>
+			<div>
+				<div class="col-md-7">
+				<div id="bid-revenue-chart" style="height: 370px; width: 100%;"></div>
+				</div>  
+			</div>    
+			<div>
 				<div class="col-md-2">
 				<?php 
-				$total = $bidsaccept;
+				$total = $bid_accepted;
 				?>
 				<div class="upgrade-revenue-price-box"><?=number_format($total)?> </div>
 				</div>  
@@ -104,7 +124,7 @@ $dataPoints = array(
 <script type="text/javascript" src="<?php echo base_url('assets/inilabs/jquery.redirect.js'); ?>"></script>
 <script>
 
-			$('#progress-<?=$bidsaccept?>').pieChart({
+			$('#progress-<?=$bid_accepted?>').pieChart({
                 barColor: '#5499C7',
                 trackColor: '#eee',
                 lineCap: 'round',
@@ -125,7 +145,7 @@ $dataPoints = array(
 <script>
 window.onload = function () {
  
-var chart = new CanvasJS.Chart("chartContainer", {
+var bvchart = new CanvasJS.Chart("bid-volume-chart", {
 	title: {
 		text: "Bid Volume"
 	},
@@ -133,12 +153,50 @@ var chart = new CanvasJS.Chart("chartContainer", {
 		title: "Bid Volume"
 	},
 	data: [{
-		type: "line",
-		dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+		dataPoints: <?php echo json_encode($bid_volume_data, JSON_NUMERIC_CHECK); ?>
 	}]
 });
-chart.render();
+bvchart.render();
  
+var brchart = new CanvasJS.Chart("bid-revenue-chart", {
+	title: {
+		text: "Bid Revenue"
+	},
+	axisY: {
+		title: "Bid Revenue"
+	},
+	data: [{
+		dataPoints: <?php echo json_encode($bid_revenue_data, JSON_NUMERIC_CHECK); ?>
+	}]
+});
+brchart.render();
 }
+$(document).ready(function(){
+	$('#filter_year_to').val(<?=$to_year?>).trigger('change');	
+	$('#filter_to_month').val(<?=$to_month?>).trigger('change');	
+});
+
+$('#filter_year_to').change(function(){
+	var year = <?=date('Y')?>;
+	var month = <?=date('m')?>;
+	if(year == $(this).val()){		
+		start_month = month-1; 
+	} else {
+		start_month = 0;
+	}
+   	end_month = 11;
+	var html = '',value=0;
+	var from_selected = '';
+	var to_selected = '';
+	var to_html = "<option value='0'>To Month</option>";
+	for(var i=start_month;i<=end_month;i++){
+		value = i+1;
+		to_selected = ('<?=$to_month?>'==value)?'selected':'';		
+		to_html += '<option value="'+value+'" '+to_selected+'>'+new Date($(this).val(),i).toLocaleString("default", { month: "long" })+'</option>';
+	}	
+	
+	
+	$('#filter_to_month').html(to_html);	
+});
 </script>
 
