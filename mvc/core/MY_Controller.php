0664c28db9b7b57e9cafb8b6656449547811c9f3
rajesh gullipalli <rajesh.gullipalli@sweken.com>
2021-06-04 19:43:57 +0530

    mail_templates changes

--- mvc/core/MY_Controller.php
+++ mvc/core/MY_Controller.php
@@ -48,8 +48,16 @@ class MY_Controller extends CI_Controller {
 	
 	public function sendMail($data){
 		 $this->mydebug->debug($data);
+		// print_r($data); exit;
+		if($data['product_id']==1)
+		{
+			$upgrade_offer=1;
+		}
+		else if($data['product_id']==2){
+			$baggage_offer=1;
+		}
+		$maildata=array();
 		 $template = 'bid_cancel';
-		 
 		  $this->load->model('airline_m');
 		 $template_images = $this->airline_m->getImagesByType($data['carrier']);
 		   foreach($template_images as $img){
@@ -64,33 +72,78 @@ class MY_Controller extends CI_Controller {
 		   }
 
           $data['mail_header_color'] = $airline_info->mail_header_color;
+
 		   if(empty($data['mail_header_color'])){
 			 $data['mail_header_color'] = '#333';  
 		   }
+		   $dir = "assets/mail-temps";
+	   $tpl = $this->mailandsmstemplate_m->getDefaultMailTemplateByCat($data['template'],$data['airlineID'])->template;
+		if ( empty($tpl) ) {
+			//Check any customized tpl file for this carrier 
+			$tpl_file = $this->mailandsmstemplate_m->getDefaultMailTemplateByCat($data['template'],$data['airlineID'])->template_path;
+
+			$t = explode('.',$tpl_file);
+		$tpl_path = $t[0]. '-imgs';
+			
+		if ( $upgrade_offer) {
+                 $maildata['up_tpl_bnr'] = base_url("$dir/$tpl_path/banner.jpg");
+                 $maildata['up_tpl_bnrtop'] = base_url("$dir/$tpl_path/bannerTop.png");	
+                 $maildata['up_tpl_bnrbottom'] = base_url("$dir/$tpl_path/bannerBottom.png");
+                
+		}
+
+		if( $baggage_offer) {
+                 $maildata['bg_tpl_bnr'] = base_url("$dir/$tpl_path/banner.png");	
+                 $maildata['bg_tpl_bnrtop'] = base_url("$dir/$tpl_path/bannerTop.png");	
+                 $maildata['bg_tpl_bnrbottom'] = base_url("$dir/$tpl_path/bannerBottom.png");	
+              
+		}
+                 $maildata['open_browser'] = base_url("$dir/$tpl_path/openBrowser.png");
+                 $maildata['facebook_icon'] = base_url("$dir/$tpl_path/facebook.png");		
+                 $maildata['tag_img'] = base_url("$dir/$tpl_path/tag.png");		
+                 $maildata['twitter_icon'] = base_url("$dir/$tpl_path/twitter.png");	
+                 $maildata['pinterest_icon'] = base_url("$dir/$tpl_path/pinterest.png");	
+                 $maildata['openbrowser_img'] = base_url("$dir/openBrowser.png");
+            	 $maildata['airline_logo'] = base_url('uploads/images/'.$this->airline_m->getAirlineLogo($maildata['airlineID'])->logo);            
+                
+                 $maildata['domain'] = $primary_client->domain;
+                 $maildata['primary_phone'] = $primary_client->phone;
+                 $maildata['primary_mail'] = $primary_client->email;
+                 $maildata['fb_link'] = "https://facebook.com";
+                 $maildata['twitter_link'] = "https://twitter.com";
+                 $maildata['pinterest_link'] = "https://pinterest.com";
+                 $maildata['unsubscribe_link'] = base_url('home/index');
+                 $maildata['forward_friend_link'] = base_url('home/index');
+                 $maildata['openbrowser_link'] = base_url('home/index');
+                 $maildata['not_intrested_link'] = base_url('home/index');
+                 
+		   $maildata['base_url'] = base_url(); 
+		   $maildata['bidnow_link'] = base_url('homes/bidding?pnr_ref='.$pnr_ref);
+		   $template_images = $this->airline_m->getImagesByType($maildata['airlineID']);
+		   foreach($template_images as $img){
+			   $maildata[$img->type] = base_url('uploads/images/'.$img->image);
+		   } 
+                   $airline_info = $this->bid_m->getAirlineLogoByPNR($pnr_ref);
+		   $maildata['mail_header_color'] = $airline_info->mail_header_color;
+		   if(empty($maildata['mail_header_color'])){
+			 $maildata['mail_header_color'] = '#333';  
+		   }
 		   
-	   $tpl = $this->mailandsmstemplate_m->getDefaultMailTemplateByCat($data['template'],$data['carrier'])->template;
-	   $message = $this->parser->parse_string($tpl, $data,TRUE);
-#echo $message;exit;
-	   // $this->mydebug->debug($message);
-	  $message =html_entity_decode($message);
-	  $siteinfos = $this->reset_m->get_site();  	     
-		 $config['protocol']='smtp';
-		 $config['smtp_host']='mail.sweken.com';
-		 $config['smtp_port']='26';
-		 $config['smtp_timeout']='30';
-		 $config['smtp_user']='info@sweken.com';
-		 $config['smtp_pass']='Infoinfo-1!';
-		 $config['charset']='utf-8';
-		 $config['newline']="\r\n";
-		 $config['wordwrap'] = TRUE;
-		 $config['mailtype'] = 'html';
-		 $this->email->initialize($config);
-		$this->email->set_mailtype("html");
-		$this->email->from($siteinfos->email,$siteinfos->sname);
-		$this->email->to($data['tomail']);
-		$this->email->subject($data['subject']);
-		$this->email->message($message);
-	   $status =  $this->email->send();
+		   if(isset($maildata['bid_value'])){
+			   $maildata['bid_value'] = number_format($maildata['bid_value']);
+		   }
+
+			$dir = "mvc/views/mail-templates";
+			#$dir = "mvc/views/offer-email-temps";
+			$tpl_path = "$dir/$tpl_file";
+				if (is_file($tpl_path)){
+				$tpl = file_get_contents($tpl_path);
+			} else {
+				$this->mydebug->debug("Template file  - $tpl_path - missing!");
+			}
+		}
+		$data=array_merge($data,$maildata);
+		$status=$this->sendMailTemplateParser($data['template'],$data);
        return $status;
    }
 
