00cb6b0d61802a095b2455f76f5b38dc14abf45c
rajesh gullipalli <rajesh.gullipalli@sweken.com>
2021-06-30 16:38:13 +0530

    db file changes for airline contract

--- db/db_changes_30-06-21.sql
+++ db/db_changes_30-06-21.sql
@@ -1,1 +1,9 @@
 ALTER TABLE `VX_contract_products` CHANGE modify_date modify_date DATETIME DEFAULT NULL;
+
+ALTER TABLE `VX_contract_products` CHANGE start_date start_date DATETIME DEFAULT NULL;
+
+ALTER TABLE `VX_contract_products` CHANGE end_date end_date DATETIME DEFAULT NULL;
+
+ALTER TABLE `VX_contract_products` CHANGE create_date create_date DATETIME DEFAULT NULL;
+
+ALTER TABLE VX_contract_products MODIFY no_users VARCHAR(11);
